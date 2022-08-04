44707dd38ef3733411839aea928c83205f831fd9
codesyariah122 <pujiermanto@gmail.com>
2022-08-04 13:04:08 +0700

    portfolio acuy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
+    <h1>Acuy Opik Website</h1>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <blockquote>
+      Ini adalah portfolio milik Acuy
+    </blockquote>
+    <small>
+      Sedang proses pengembangan
+    </small>
   </div>
 </template>
 
